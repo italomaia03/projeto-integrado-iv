054dcc2d68d4ea507af3f36839c5bc913df990d4
Ítalo F. O. Maia <109195782+italomaia03@users.noreply.github.com>
2024-07-04 17:41:58 -0300

    feat: adicionar páginas de login e registro

---
+++ lib/login_view.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              AppBar(
+                title: const Text('Login'),
+              ),
+              const SizedBox(height: 20.0),
+              Center(
+                child: Image.asset(
+                  'assets/icons/somar-logo.png',
+                  height: 100.0,
+                ),
+              ),
+              const SizedBox(height: 50.0),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 20.0),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 4.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Checkbox(value: false, onChanged: (bool? value) {}),
+                      const Text('Remember Password'),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Forgot my password'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                ),
+                child:
+                    const Text('Login', style: TextStyle(color: Colors.white)),
+              ),
+              const SizedBox(height: 20.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: const Text('Register now'),
+              ),
+              const SizedBox(height: 20.0),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const SizedBox(height: 10),
+                    IconButton(
+                      icon: const FaIcon(FontAwesomeIcons.google),
+                      onPressed: () => true,
+                      iconSize: 48.0,
+                    ),
+                    const SizedBox(height: 10),
+                    IconButton(
+                      icon: const FaIcon(FontAwesomeIcons.facebook),
+                      onPressed: () => true,
+                      iconSize: 48.0,
+                    ),
+                    const SizedBox(height: 10),
+                    IconButton(
+                      icon: const FaIcon(FontAwesomeIcons.apple),
+                      onPressed: () => true,
+                      iconSize: 48.0,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 112.0),
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Terms and Policies of Privacy'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  _RegisterViewState createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _repeatPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    // Implement photo upload functionality here
+                  },
+                  child: const CircleAvatar(
+                    radius: 50,
+                    child: Icon(Icons.camera_alt),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'User Functional ID',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Function',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'First Name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Last Name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an email';
+                    }
+                    String pattern = r'\w+@\w+\.\w+';
+                    RegExp regex = RegExp(pattern);
+                    if (!regex.hasMatch(value)) {
+                      return 'Enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _repeatPasswordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Repeat Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10.0),
+                Row(
+                  children: <Widget>[
+                    Checkbox(value: false, onChanged: (bool? value) {}),
+                    const Expanded(
+                      child: const Text('I agree to the terms of privacy'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Implement registration logic here
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  child: const Text('Register',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
