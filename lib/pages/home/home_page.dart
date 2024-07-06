b53bf1c4acc4b390fc44924a5933eb02db302646
fcoeliesio <fcoeliesio@gmail.com>
2024-07-06 15:00:40 -0300

    Adicionando botão para inserir mais produtos

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:somar/bd/database.dart';
+import 'package:somar/models/produtor.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,23 +20,69 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount:  Database().produtos.length,
-              itemBuilder: (context, index){
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Container(
-                    height: 50,
-                    color: Color.fromARGB(255, 218, 218, 218),
-                  ),
-                );
-              }),
-          ), 
-          ],
+        child: Expanded(
+          child: Column(
+            children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount:  Database().produtos.length,
+                itemBuilder: (context, index){
+                  var item = Database().produtos.elementAt(index);
+          
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Expanded(
+                      child: Material(
+                        child: InkWell(
+                          onTap: (){},
+                          child: Ink(
+                            color: Color.fromARGB(255, 218, 218, 218),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const SizedBox(width: 15),
+                                const Icon(Icons.calendar_month_outlined),
+                                const SizedBox(width: 20),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text(item.titulo, style: const TextStyle(
+                                        fontWeight: FontWeight.w400, fontSize: 16,
+                                      ),),
+                                      Text('Quandidade: ${item.quantidade.toString()}',
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+            ), 
+            ],
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            Database().addProduto(Produto(
+              codigo: Database().produtos.length+1,
+              data: 'teste',
+              titulo: 'Título teste',
+              quantidade: 10,
+            ));
+          },
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
