c3abbc865d5ad05f53ca670e765f500c0e8fab5a
fcoeliesio <fcoeliesio@gmail.com>
2024-07-06 14:10:43 -0300

    Inserindo elementos da home

--- lib/bd/database.dart
+++ lib/bd/database.dart
@@ -1,3 +1,4 @@
+import 'package:somar/models/produtor.dart';
 import 'package:somar/models/usuario.dart';
 
 class Database{
@@ -12,11 +13,40 @@ class Database{
   List<Usuario> usuarios = [
     Usuario(
       codigo: 40028922,
-      nome: 'fabricio',
+      nome: 'Fabrício Freitas',
       email: 'fabricio@gmail.com',
       senha: 'naruto',
     ),
   ];
+
+  List<Produto> produtos = [
+    Produto(
+      codigo: 1, 
+      data: 'data', 
+      titulo: 'Vinho padre Cícero 880mL Tinto', 
+      quantidade: 100,
+      ),
+      Produto(
+      codigo: 2, 
+      data: 'data', 
+      titulo: 'Oléo para pernas padre Cícero', 
+      quantidade: 100,
+      ),
+      Produto(
+      codigo: 3, 
+      data: 'data', 
+      titulo: 'Pomada padre Cícero', 
+      quantidade: 200,
+      )
+  ];
+
+  void addProduto(Produto produto){
+    produtos.add(produto);
+  }
+
+  void removeProduto(int index){
+    produtos.removeAt(index);
+  }
 
   Usuario? login(String email, String senha){
     for(var usuario in usuarios){
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:somar/bd/database.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +12,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[300],
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
-        title: Text('Início'),
+        title: Text(Database().usuarioLogado?.nome ?? ''),
+        backgroundColor: Colors.blue[400],
       ),
-      body: Container(
-        
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount:  Database().produtos.length,
+              itemBuilder: (context, index){
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Container(
+                    height: 50,
+                    color: Color.fromARGB(255, 218, 218, 218),
+                  ),
+                );
+              }),
+          ), 
+          ],
+        ),
       ),
     );
   }
