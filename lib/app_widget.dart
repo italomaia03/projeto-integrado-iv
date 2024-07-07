4c444deb09e2165cc5053f2354abcfd851fe40b9
Ítalo F. O. Maia <109195782+italomaia03@users.noreply.github.com>
2024-07-07 18:34:43 -0300

    feat: injetar repositório nas páginas de login e registro

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:somar/controllers/auth_controller.dart';
 import 'package:somar/pages/home/home_page.dart';
 import 'package:somar/pages/login/login_page.dart';
 import 'package:somar/pages/produtos/protudo_page.dart';
 import 'package:somar/pages/splash/splash_page.dart';
+import 'package:somar/repository/user_repository.dart';
 import 'package:somar/routes.dart';
 import 'pages/register/register.dart';
 
 class AppWidget extends StatelessWidget{
-  const AppWidget({super.key});
-
+  AppWidget({super.key});
+  final UserRepository userRepository = UserRepository();
   @override
   Widget build(BuildContext context){
     return MaterialApp(
@@ -18,8 +20,8 @@ class AppWidget extends StatelessWidget{
       ),
       routes: {
         Routes.initial: (context) => const SplashPage(),
-        Routes.login: (context) => const LoginPage(),
-        Routes.register: (context) => const RegisterView(),
+        Routes.login: (context) => LoginPage(authController: AuthController(userRepository)),
+        Routes.register: (context) => RegisterView(authController: AuthController(userRepository)),
         Routes.home: (context) => const HomePage(),
         Routes.produtos: (context) => const ProdutoPage(),
       },
