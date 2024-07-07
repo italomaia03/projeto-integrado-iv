2fd0e83579995e0068bf5d30ea70fb8aaca42fcc
Ítalo F. O. Maia <109195782+italomaia03@users.noreply.github.com>
2024-07-07 19:09:32 -0300

    feat: adicionar usuário para testes

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,7 +1,16 @@
 import '../models/user_model.dart';
 
 class UserRepository {
-  var users = <String, UserModel>{};
+  var users = <String, UserModel>{
+    'fabricio@gmail.com' : UserModel(
+      'Fabricio',
+      'Freitas',
+      'fabricio@gmail.com',
+      'Abcd@1234',
+      'Professor',
+      '123456'
+    )
+  };
 
   void save(UserModel model) {
     users[model.email] = model;
